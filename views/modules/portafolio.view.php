f82f4e459bf48d02975a7f0705a24b61fc1f36a0
Edsel Carvajal <edsel.melendez.carv@gmail.com>
2018-02-02 12:21:25 -0600

    Agregamos Portafolio

--- views/modules/portafolio.view.php
+++ views/modules/portafolio.view.php
@@ -1,3 +1,29 @@
 <section class="portafolio">
-	
+	<h2>PORTAFOLIO</h2>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="image">
+					<img src="<?php echo $route; ?>views/img/first-page.jpg" alt="Desarrollo Web">
+					<div class="caption">
+						<img src="<?php echo $route; ?>views/img/plus.png">
+					</div>
+				</div>
+				<h3>First Page</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates tempora soluta fuga perferendis minus adipisci ipsa ad quam voluptas esse est laboriosam quod nesciunt tenetur, quidem iusto, asperiores. Sint, inventore.</p>
+			</div>
+
+			<div class="col-12">
+				<div class="image">
+					<img src="<?php echo $route; ?>views/img/second-page.jpg" alt="Desarrollo Web">
+					<div class="caption">
+						<img src="<?php echo $route; ?>views/img/plus.png">
+					</div>
+				</div>
+				<h3>Second Page</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates tempora soluta fuga perferendis minus adipisci ipsa ad quam voluptas esse est laboriosam quod nesciunt tenetur, quidem iusto, asperiores. Sint, inventore.</p>
+			</div>
+			<a href="" class="btn-large btn-outline-white">VER MAS</a>
+		</div>
+	</div>
 </section>
