e23fe9d4b0ee6e7c43711cca76add0593d6fa819
Edsel Carvajal <edsel.melendez.carv@gmail.com>
2018-02-19 12:19:54 -0600

    Agrego de favicon

--- views/modules/footer.view.php
+++ views/modules/footer.view.php
@@ -3,12 +3,16 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-6 col-lg-3">
-					<h3>LOREM IPSUM</h3>
+					<h3>ACCESO RAPIDO</h3>
 					<ul>
-						<li><a href="#">Lorem Ipsum</a></li>
-						<li><a href="#">Lorem Ipsum</a></li>
-						<li><a href="#">Lorem Ipsum</a></li>
-						<li><a href="#">Lorem Ipsum</a></li>
+						<?php 
+							$item = null;
+							$value = null;
+							$components = SectionController::ctrShowSections($item, $value) 
+						?>
+						<?php foreach ($components as $component => $page): ?>
+							<li><a href="<?php echo $page['route'] ?>"><?php echo $page['section']; ?></a></li>
+						<?php endforeach ?>
 					</ul>
 				</div>
 
@@ -53,6 +57,7 @@
 					<a href="#"><img src="<?php echo $Route; ?>views/img/fb.png" width="30" height="30" alt="Designninweb Facebooks"></a>
 				</div>
 				<div class="col-12"><p>Copyright &copy; DIW. Todos Los Derechos Reservados</p></div>
+				<a href="https://icons8.com" class="credits">Icon pack by Icons8</a>
 			</div>
 		</div>
 	</section>
